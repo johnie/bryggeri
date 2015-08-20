6565ab486f83b3ab6ac62fba7de4b88d90d3038d
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-08-20 22:20:45 +0200

    Typo

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 install() {
-  printf "\e[33m[~] Downloading script...\e[0m\n"
+    printf "\e[33m[~] Downloading script...\e[0m\n"
 
-  curl -L#o /var/tmp/bryggeri https://raw.githubusercontent.com/johnie/bryggeri/master/bin/bryggeri
+    curl -L#o /var/tmp/bryggeri https://raw.githubusercontent.com/johnie/bryggeri/master/bin/bryggeri
 
-  printf "\n\e[33m[~] Setting permissions...\e[0m\n"
+    printf "\n\e[33m[~] Setting permissions...\e[0m\n"
 
-  chmod -v +x /var/tmp/bryggeri
+    chmod -v +x /var/tmp/bryggeri
 
-  echo
+    echo
 
-  printf "\e[33m[~] Moving to \$PATH...\e[0m\n"
+    printf "\e[33m[~] Moving to \$PATH...\e[0m\n"
 
-  sudo mv -fv /var/tmp/bryggeri /usr/local/bin/bryggeri
+    sudo mv -fv /var/tmp/bryggeri /usr/local/bin/bryggeri
 
-  echo
+    echo
 
-  version=($(bryggeri -V))
-  printf "\e[32m[✔] Successfully installed Bryggeri v${version[3]}\e[32m!\n"
+    version=($(bryggeri -V))
+    printf "\e[32m[✔] Successfully installed Bryggeri v${version[3]}\e[32m!\n"
 }
 
 install
